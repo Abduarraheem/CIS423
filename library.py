1c7d43fbcb40d341a9f58ca48fe49ee0de9777c5
Abduarraheem <57520195+Abduarraheem@users.noreply.github.com>
2022-01-12 10:24:33 -0800

    Added transformers.
    Double-check DropColumnsTransformer.

--- library.py
+++ library.py
@@ -2,3 +2,89 @@ import pandas as pd
 import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
+
+from sklearn.base import BaseEstimator, TransformerMixin #gives us the tools to build custom transformers
+
+#This class maps values in a column, numeric or categorical.
+class MappingTransformer(BaseEstimator, TransformerMixin):
+  
+  def __init__(self, mapping_column, mapping_dict:dict):  
+    self.mapping_dict = mapping_dict
+    self.mapping_column = mapping_column  #column to focus on
+
+  def fit(self, X, y = None):
+    print("Warning: MappingTransformer.fit does nothing.")
+    return X
+
+  def transform(self, X):
+    assert isinstance(X, pd.core.frame.DataFrame), f'MappingTransformer.transform expected Dataframe but got {type(X)} instead.'
+    assert self.mapping_column in X.columns.to_list(), f'MappingTransformer.transform unknown column {self.mapping_column}'
+    X_ = X.copy()
+    X_[self.mapping_column].replace(self.mapping_dict, inplace=True)
+    return X_
+
+  def fit_transform(self, X, y = None):
+    result = self.transform(X)
+    return result
+ 
+#This class will rename one or more columns.
+class RenamingTransformer(BaseEstimator, TransformerMixin):
+  #your __init__ method below
+    def __init__(self, renamed_dict:dict):  
+      self.renamed_dict = renamed_dict
+  #write the transform method without asserts. Again, maybe copy and paste from MappingTransformer and fix up.
+    def transform(self, X):
+      assert isinstance(X, pd.core.frame.DataFrame), f'RenamingTransformer.transform expected Dataframe but got {type(X)} instead.'
+      #your assert code below
+      assert set(self.renamed_dict.keys()).issubset(X.columns), f"{set(self.renamed_dict.keys())-set(X.columns)} not a column."
+      X_ = X.copy()
+      X_.rename(columns=self.renamed_dict) 
+      return X_
+
+class OHETransformer(BaseEstimator, TransformerMixin):
+  def __init__(self, target_column, dummy_na=False, drop_first=True):  
+    self.target_column = target_column
+    self.dummy_na = dummy_na
+    self.drop_first = drop_first
+  
+  #fill in the rest below
+  def fit(self, X, y = None):
+    print("Warning: OHETransformer.fit does nothing.")
+    return X
+  def transform(self, X):
+    assert isinstance(X, pd.core.frame.DataFrame), f'OHETransformer.transform expected Dataframe but got {type(X)} instead.'
+    assert self.target_column in X.columns.to_list(), f'OHETransformer.transform unknown column {self.target_column}'
+    X_ = X.copy()
+    X_ = pd.get_dummies(X_, prefix=self.target_column, prefix_sep='_', 
+                    columns=[self.target_column],
+                    dummy_na=self.dummy_na,       #will try to impute later so leave NaNs in place
+                    drop_first=self.drop_first    #really should be True but I wanted to give a clearer picture
+                  )
+    return X_
+
+  def fit_transform(self, X, y = None):
+    result = self.transform(X)
+    return result
+  
+class DropColumnsTransformer(BaseEstimator, TransformerMixin):
+  def __init__(self, column_list, action='drop'):
+    assert action in ['keep', 'drop'], f'DropColumnsTransformer action {action} not in ["keep", "drop"]'
+    assert isinstance(column_list, list), f'DropColumnsTransformer expected list but saw {type(column_list)}'
+    self.column_list = column_list
+    self.action = action
+
+  #fill in rest below
+  def fit(self, X, y = None):
+    print("Warning: DropColumnsTransformer.fit does nothing.")
+    return X
+  def transform(self, X):
+    assert isinstance(X, pd.core.frame.DataFrame), f'DropColumnsTransformer.transform expected Dataframe but got {type(X)} instead.'
+    assert set(self.column_list).issubset(X.columns), f"{set(self.column_list)-set(X.columns)} not in columns."
+    X_ = X.copy()
+    if self.action == 'drop':
+      X_ = X_.drop(columns=self.column_list)
+    return X_
+
+  def fit_transform(self, X, y = None):
+    result = self.transform(X)
+    return result
